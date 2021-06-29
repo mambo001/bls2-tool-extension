ecc4b8237bfb611d891df993e1c59a41e9d6cf1b
Reuben Mark Gorda <reuben.gorda@gmail.com>
2021-06-30 06:44:31 +0800

    Cleaned up fetch url and added post body data to parameters for backup purposes.

--- js/background.js
+++ js/background.js
@@ -4,9 +4,11 @@ chrome.runtime.onMessage.addListener(
           console.log("Query: submittedBucketQuery")
             
           // Submitted Dashboard Cases Query
+          const caseData = encodeURIComponent(JSON.stringify(request.postData));
           const bls2ToolEndpointURL = `https://script.google.com/a/macros/google.com/s/AKfycbxivFmpPEs3So4NaC3Lk2BN5iE3ahxF4H4eTt8W-v64/dev`;
+          const URL = `${bls2ToolEndpointURL}?caseData=${caseData}`
             
-          fetch(bls2ToolEndpointURL,{
+          fetch(URL,{
               method: 'POST',
               cache: 'no-cache',
               redirect: 'follow',
